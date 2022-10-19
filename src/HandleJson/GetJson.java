051e26aeb609852b578b5fffbdc47fa0d6612811
RoniDeringer <ronideringer@gmail.com>
2022-10-18 22:10:17 -0300

    get Json

--- src/HandleJson/GetJson.java
+++ src/HandleJson/GetJson.java
@@ -2,5 +2,11 @@ package HandleJson;
 
 public class GetJson {
 	//java JACKSON
-		//Mapping a Dynamic JSON Object with Jackson | Baeldung
+	//Mapping a Dynamic JSON Object with Jackson | Baeldung
+
+	
+	
+	//pegar o json e usar o setNome()
+	
+	
 }
---
+++ src/HandleJson/TestSimulaJson.java
@@ -0,0 +1,8 @@
+package HandleJson;
+
+public class TestSimulaJson {
+
+	public String json = " {\r\n"
+			+ "    \"nome\": \"teste1\"\r\n }";
+
+}
